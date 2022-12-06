0436969c19d82bb902e86cf406630cd13adb7ca3
Prasanna Aithal <prasanna.aithal@gonuclei.com>
2022-12-06 06:47:41 +0530

    DUAT

---
+++ BuildAutomate/BuildAutomate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuildAutomate
+//
+//  Created by Prasanna on 04/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
