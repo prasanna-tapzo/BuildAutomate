d35afa418991ac7c387659af6344767bd07cf58b
Prasanna Aithal <prasanna.aithal@gonuclei.com>
2022-12-06 11:59:50 +0530

    DUAT

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -6,6 +6,7 @@ gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/RichNotificationService.mobileprovision ./.github/secrets/RichNotificationService.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/RichNotificationServiceDev.mobileprovision ./.github/secrets/RichNotificationServiceDev.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Notification_Content_Extension_Distribution_2022D.mobileprovision ./.github/secrets/Notification_Content_Extension_Distribution_2022D.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/FedMobileDCDev.mobileprovision ./.github/secrets/FedMobileDCDev.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
@@ -15,7 +16,7 @@ cp ./.github/secrets/Notification_Content_Extension_Distribution_2022.mobileprov
 cp ./.github/secrets/RichNotificationService.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/RichNotificationService.mobileprovision
 cp ./.github/secrets/RichNotificationServiceDev.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/RichNotificationServiceDev.mobileprovision
 cp ./.github/secrets/Notification_Content_Extension_Distribution_2022D.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Notification_Content_Extension_Distribution_2022D.mobileprovision
-
+cp ./.github/secrets/FedMobileDCDev.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/FedMobileDCDev.mobileprovision
 
 security create-keychain -p "" build.keychain
 security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "123456" -A
